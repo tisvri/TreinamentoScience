9498c7a879e6e1322693671f0bd46ae5a73d7c30
tisvri <102690407+tisvri@users.noreply.github.com>
2024-02-22 16:42:31 -0300

    TreinamentosScience v2
    Atualizações:
    - Foi feita a separação de integração e treinamentos
    - Trocado os links dos forms da microsoft
    - Adição no css
    - Adição de novas paginas

--- code.js
+++ code.js
@@ -1,34 +1,28 @@
 //Botões para ir aos cursos//
-document.getElementById("Botaolgpd").onclick = function() {
-    window.location.href = "treinamentolgpd/TreinamentosLGPD.html";
+document.getElementById("Botaolgpd").onclick = function () {
+    window.location.href = "/treinamentolgpd/TreinamentosLGPD.html";
 };
 
-document.getElementById("Botaotecnologia").onclick = function() {
-    window.location.href = "treinamentotecnologia/hubtecnologia.html"
+document.getElementById("Botaotecnologia").onclick = function () {
+    window.location.href = "/treinamentotecnologia/hubtecnologia.html"
 };
 
-document.getElementById("Botaoregulatorio").onclick = function() {
-    window.location.href = "treinamentoregulatorio/TreinamentoRegulatorio.html";
+document.getElementById("Botaoregulatorio").onclick = function () {
+    window.location.href = "/treinamentoregulatorio/TreinamentoRegulatorio.html";
 };
 
-document.getElementById("Botaoqualidade").onclick = function() {
-    window.location.href = "treinamentoqualidade/TreinamentoQualidade.html";
+document.getElementById("Botaoqualidade").onclick = function () {
+    window.location.href = "/treinamentoqualidade/TreinamentoQualidade.html";
 };
 
-document.getElementById("Botaorh").onclick = function() {
-    window.location.href = "treinamentocapitalhumano/TreinamentoCapitalHumano.html";
+document.getElementById("Botaorh").onclick = function () {
+    window.location.href = "/treinamentocapitalhumano/TreinamentoCapitalHumano.html";
 };
 
-document.getElementById("BotaoAcademy").onclick = function() {
-    window.location.href = "treinamentoacademy/TreinamentoAcademy.html";
+document.getElementById("BotaoAcademy").onclick = function () {
+    window.location.href = "/treinamentoacademy/TreinamentoAcademy.html";
 };
 
-//Treinamentos LGPD//
-document.getElementById("aula2").onclick = function() {
-    window.location.href = "TreinamentosLGPD2.html";
+document.getElementById("Botaoindex").onclick = function () {
+    window.location.href = "/index.html"
 };
-
-document.getElementById("aula3").onclick = function() {
-    window.location.href = "TreinamentosLGPD3.html";
-};
-
