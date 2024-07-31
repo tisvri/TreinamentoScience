683f37ed7ebc468291d6f77ef589fd5ef341557c
Victor <ti@svriglobal.com>
2024-07-31 10:43:26 -0300

    Treinamentos Science v4

---
+++ assets/js/TreinamentosLGPD.js
@@ -0,0 +1,7 @@
+
+document.getElementById("lgpd-aula1").onclick = function () {
+    window.location.href = "TreinamentosLGPD2.html";
+};
+
+
+
---
+++ assets/js/centralTreinamentos.js
@@ -0,0 +1,44 @@
+document.getElementById("BotaoIntegração").onclick = function () {
+    window.location.href = "../index.html"
+};
+
+document.getElementById("Botaocontroladoria").onclick = function () {
+    window.location.href = "https://netorgft4487772.sharepoint.com/:f:/s/ScienceValley/EkSziWM14z5Ft-SR-6rLyPsBFWjCaMNh_zW1U915B9oPWA?e=skPrkw"
+};
+
+document.getElementById("Botaodiretrizes").onclick = function () {
+    window.location.href = "../politicas/politicasdone.html"
+};
+
+document.getElementById("Botaotecnologia").onclick = function () {
+    window.location.href = "../integracaoti/IntegracaoTI.html"
+};
+
+document.getElementById("Botaolgpd").onclick = function () {
+    window.location.href = "../treinamentolgpd/TreinamentosLGPD.html";
+};
+
+document.getElementById("BotaoCTMS").onclick = function () {
+    window.location.href = "../TreinamentoCtms/TreinamentoCtms.html"
+};
+
+document.getElementById("Botaotecnologiacentral").onclick = function () {
+    window.location.href = "../treinamentotecnologia/hubtecnologia.html"
+};
+
+document.getElementById("Botaoqualidade").onclick = function () {
+    window.location.href = "../treinamentoqualidade/TreinamentoQualidade2.html";
+};
+
+document.getElementById("BotaoAcademy").onclick = function () {
+    window.location.href = "../treinamentoacademy/TreinamentoAcademy.html";
+};
+
+document.getElementById("Botaopop").onclick = function () {
+    window.location.href = "../secao_pops/centralpops.html"
+};
+
+document.getElementById("BotaoFinanceiroPolo").onclick = function () {
+    window.location.href = "../Treinamento_Financeiro_Polotrial/TreinamentoFinanceiroPolotrial_Aula1.html"
+};
+
